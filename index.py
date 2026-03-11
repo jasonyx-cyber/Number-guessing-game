78b1f02d1faa43febeee23db4a2f25596761fa35
jasonyx-cyber <jasonokpor@gmail.com>
2026-03-11 14:41:15 +0100

    first commit: number guessing game

---
+++ index.py
@@ -0,0 +1,14 @@
+import random
+
+secret_number = random.randint(1, 100)
+guess = 0
+
+while guess != secret_number:
+    guess = int(input("Guess the number between 1 and 100: "))
+    if guess < secret_number:
+        print("Too low! Try again,.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You've guessed the number.")
+        
